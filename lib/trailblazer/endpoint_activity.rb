0376feee4dcbf9c266e80e6baef7b31db1ab85dc
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-08-12 19:44:00 +0800

    WIP - playing with circut interface

--- lib/trailblazer/endpoint_activity.rb
+++ lib/trailblazer/endpoint_activity.rb
@@ -1,83 +1,91 @@
 module Trailblazer
-  class EndpointActivity < Trailblazer::Activity::Path
-    step :created, Output(Activity::Right, :success) => Id(:render), Output(Activity::Left, :failure) => Track(:success)
-    step :deleted, Output(Activity::Right, :success) => Id(:render), Output(Activity::Left, :failure) => Track(:success)
-    step :found, Output(Activity::Right, :success) => Id(:render), Output(Activity::Left, :failure) => Track(:success)
-    step :success, Output(Activity::Right, :success) => Id(:render), Output(Activity::Left, :failure) => Track(:success)
-    step :unauthenticated, Output(Activity::Right, :success) => Id(:render), Output(Activity::Left, :failure) => Track(:success)
-    step :not_found, Output(Activity::Right, :success) => Id(:render), Output(Activity::Left, :failure) => Track(:success)
-    step :invalid, Output(Activity::Right, :success) => Id(:render), Output(Activity::Left, :failure) => Track(:success)
-    step :fallback, Output(Activity::Left, :failure) => Track(:success)
-    step :render
+  # NOTE: Expects to receive the result of an operation and the representer
+  # class as params in the ctx. This endpoint will then mutate the context
+  # to add the represented data and the matched result that can be used for
+  # render
+  class EndpointActivity < Trailblazer::Activity::Railway
+    step :created, Output(Activity::Right, :success) => End(:created), Output(Activity::Left, :failure) => Track(:success)
+    # step :deleted, Output(Activity::Right, :success) => Id(:render), Output(Activity::Left, :failure) => Track(:success)
+    # step :found, Output(Activity::Right, :success) => Id(:render), Output(Activity::Left, :failure) => Track(:success)
+    # step :success, Output(Activity::Right, :success) => Id(:render), Output(Activity::Left, :failure) => Track(:success)
+    # step :unauthenticated, Output(Activity::Right, :success) => Id(:render), Output(Activity::Left, :failure) => Track(:success)
+    # step :not_found, Output(Activity::Right, :success) => Id(:render), Output(Activity::Left, :failure) => Track(:success)
+    # step :invalid, Output(Activity::Right, :success) => Id(:render), Output(Activity::Left, :failure) => Track(:success)
+    step :fallback, Output(Activity::Right, :success) => End(:fallback)
 
     private
 
-    def created(options, activity:, representer:, **)
-      return false unless activity.success? && activity["model.action"] == :new
-
-      options[:result] = { "data": representer.new(activity[:model]), "status": :created }
+    def created((ctx, flow_options), *)
+      activity = ctx[:activity]
+      if activity.success? && activity["model.action"] == :new
+        signal = Trailblazer::Activity::Right
+        representer = ctx[:representer]
+        ctx = ctx.merge(
+          result: { data: representer.new(activity[:model]),
+                    status: :created }
+        )
+      else
+        signal = Trailblazer::Activity::Left
+      end
+      [signal, [ctx, flow_options]]
     end
 
-    def deleted(options, activity:, **)
-      return false unless activity.success? && activity["model.action"] == :destroy
+    # def deleted(options, activity:, **)
+    #   return false unless activity.success? && activity["model.action"] == :destroy
 
-      options[:result] = { "data": { id: activity[:model].id }, "status": :ok }
-    end
+    #   options[:result] = { "data": { id: activity[:model].id }, "status": :ok }
+    # end
 
-    def found(options, activity:, representer:, **)
-      return false unless activity.success? && activity["model.action"] == :find_by
+    # def found(options, activity:, representer:, **)
+    #   return false unless activity.success? && activity["model.action"] == :find_by
 
-      options[:result] = { "data": representer.new(activity[:model]), "status": :ok }
-    end
+    #   options[:result] = { "data": representer.new(activity[:model]), "status": :ok }
+    # end
 
-    def success(options, activity:, **)
-      return false unless activity.success?
+    # def success(options, activity:, **)
+    #   return false unless activity.success?
 
-      representer = options[:representer]
-      data = if representer
-               representer.new(activity[:model])
-             else
-               activity[:model]
-             end
+    #   representer = options[:representer]
+    #   data = if representer
+    #            representer.new(activity[:model])
+    #          else
+    #            activity[:model]
+    #          end
 
-      options[:result] = { "data": data, "status": :ok }
-    end
+    #   options[:result] = { "data": data, "status": :ok }
+    # end
 
-    def unauthenticated(options, activity:, **)
-      return false unless activity.policy_error?
+    # def unauthenticated(options, activity:, **)
+    #   return false unless activity.policy_error?
 
-      options[:result] = { "data": {}, "status": :unauthorized }
-    end
+    #   options[:result] = { "data": {}, "status": :unauthorized }
+    # end
 
-    def not_found(options, activity:, **)
-      return false unless activity.failure? && activity["result.model"]&.failure?
+    # def not_found(options, activity:, **)
+    #   return false unless activity.failure? && activity["result.model"]&.failure?
 
-      options[:result] = {
-        "data": { errors: activity["result.model.errors"] },
-        "status": :unprocessable_entity
-      }
-    end
+    #   options[:result] = {
+    #     "data": { errors: activity["result.model.errors"] },
+    #     "status": :unprocessable_entity
+    #   }
+    # end
 
-    def invalid(options, activity:, **)
-      return false unless activity.failure?
+    # def invalid(options, activity:, **)
+    #   return false unless activity.failure?
 
-      options[:result] = {
-        "data": { errors: activity.errors || activity[:errors] },
-        "status": :unprocessable_entity
-      }
-    end
+    #   options[:result] = {
+    #     "data": { errors: activity.errors || activity[:errors] },
+    #     "status": :unprocessable_entity
+    #   }
+    # end
 
-    def fallback(options, _activity:, **)
-      options[:result] = {
-        "data": { errors: "Can't process the result" },
-        "status": :unprocessable_entity
-      }
-    end
-
-    def render(options, **)
-      # TODO: Think about the api for this endpoint
-      # should this make use of the respond_to or just return and let the
-      # controller make use of the result of this activity?
+    def fallback((ctx, flow_options), *)
+      signal = Trailblazer::Activity::Right
+      ctx = ctx.merge(
+        result: { "data": { errors: "Can't process the result" },
+                  "status": :unprocessable_entity }
+      )
+      [signal, [ctx, flow_options]]
     end
   end
 end
